f5c57cfd8f1930790560e5361ae97cebdb0506bb
Ganesh <149141995+ganesh-badar@users.noreply.github.com>
2026-02-15 23:57:36 +0530

    Add basic conditional statements in basic2.js

---
+++ JavaScript/Basic/basic2.js
@@ -0,0 +1,24 @@
+let x = 10;
+let y = 20;
+if(x>9 && y<24 ){
+    console.log("A");
+    
+}
+else {
+    console.log("B");
+}
+
+
+let isAdmin = true;
+let isLoggedin = false;
+
+if(isAdmin || isLoggedin){
+    console.log("Access Granted");
+}
+else{
+    console.log("Access Denied");
+}
+
+o/p-->
+//A
+//Access Granted
